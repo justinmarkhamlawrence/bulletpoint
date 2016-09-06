f4ba004e60bdf6919cbaf0bef193667578f11d47
dillon-mckee <dillon.mckee@gmail.com>
2016-09-06 17:25:48 -0400

    added backend

---
+++ js/inbox.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var actions = require('./actions');
+var connect = require('react-redux').connect;
+
+/**
+* Input constructor
+* @namespace InputContainer
+* @constructor
+* Renders an input box with a submit button to allow user to make a game guess
+* Allows user to input a guess into the game that gets dispatched to an action
+*/
+var InputContainer = React.createClass({
+  onGuessSubmit: function(event) {
+      event.preventDefault();
+        var userNum = parseInt(this.refs.userInput.value);
+        this.props.dispatch(actions.makeGuess(userNum));
+        this.props.dispatch(
+            actions.fetchDescription(this.props.repository.name)
+        );
+        this.refs.userInput.value = null;
+    },
+
+    render: function(){
+      return(
+        <div className="inputField">
+            <form onSubmit={this.onGuessSubmit}>
+              <input type="text" ref="userInput" /><button name="input-button">Guess</button>
+              </form>
+          </div>)
+    }
+});
--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-var $ = require('jquery');
-var makeBackgroundRed = require('./background');
-var todos = [];
+/** Declaring dependencies */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Provider = require('react-redux').Provider;
 
+var store = require('./store');
 
-$(document).ready(function() {
-    makeBackgroundRed();
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+      <Provider store={store}>
+      </Provider>,
+      document.getElementById('app')
+    );
 });
