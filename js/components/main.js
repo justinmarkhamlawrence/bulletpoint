277a2c491bd5123fdd7d70ecc3f2ae0291deef0c
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-09-06 22:30:54 -0400

    initial commit on branch

---
+++ js/components/main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div className='hi'>Hello, it is the World that belongs to Jesse!</div>
+    )
+  }
+})
+
+module.exports = Main;
